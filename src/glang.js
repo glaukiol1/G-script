80674fe306dfd808b44e6687bef8babed2ad6b93
glaukiol1 <glaukiol1@gmail.com>
2021-05-11 19:37:51 +0200

    Add some stuff to main file

--- src/glang.js
+++ src/glang.js
@@ -6,3 +6,30 @@
  * This is where all of the files will come together
  * to run a .g file.
  */
+
+const fs = require('fs')
+const path = require('path')
+
+class Interpreter {
+    constructor(file_name,options) {
+        this.options = options;
+        this.options.indexOf('verbose')!==-1?console.log('Reading File'):''
+        this.file = fs.readFileSync(path.join(__dirname, file_name), 'utf8')
+        this.options.indexOf('verbose')!==-1?console.log('Read File'):''
+        this.main()
+    }
+
+    main = () => {
+        console.log(this.file)
+    }
+}
+
+const pvg = process.argv;
+const pvgl = process.argv.length
+const opt = pvg[pvgl-2];
+
+if (pvg[pvgl-1].indexOf('.g')!==-1) {
+    new Interpreter(pvg[pvgl-1]);
+} else {
+    throw Error(`The argument at position [-1] does not contain ".g"! FATAL`)
+}
