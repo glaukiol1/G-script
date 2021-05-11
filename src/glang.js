774f215dc91a8e95f32cbbaea544e11800a4d834
glaukiol1 <glaukiol1@gmail.com>
2021-05-11 19:54:42 +0200

    Update main file

--- src/glang.js
+++ src/glang.js
@@ -19,8 +19,24 @@ class Interpreter {
         this.main()
     }
 
-    main = () => {
-        console.log(this.file)
+    lineTypeHandler(line) {
+        let variableRegex = /^var .* = .*$/i;
+        if(variableRegex.test(line)) {
+            console.log('Variable!')
+        } else {
+            console.log('Not a variable')
+        }
+    }
+
+    variableHandler(line) {
+        console.log(line)
+    }
+
+    main() {
+        const lines = this.file.split('\n')
+        lines.forEach(line=>{
+            this.lineTypeHandler(line)
+        })
     }
 }
 
@@ -29,7 +45,7 @@ const pvgl = process.argv.length
 const opt = pvg[pvgl-2];
 
 if (pvg[pvgl-1].indexOf('.g')!==-1) {
-    new Interpreter(pvg[pvgl-1]);
+    new Interpreter(pvg[pvgl-1], opt);
 } else {
     throw Error(`The argument at position [-1] does not contain ".g"! FATAL`)
 }
