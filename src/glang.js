c7b4f6320e5d7109373098b50e64584d4a1088b7
glaukiol1 <glaukiol1@gmail.com>
2021-05-11 21:36:54 +0200

    Major changes and imporovements

--- src/glang.js
+++ src/glang.js
@@ -14,6 +14,7 @@
 const path = require('path')
 const fs = require('fs')
 const Variable = require('./Variable')
+const Type = require('./Type')
 
 class Interpreter {
     constructor(file_name,options) {
@@ -25,20 +26,28 @@ class Interpreter {
         this.main()
     }
 
-    lineTypeHandler(line) {
+    lineTypeHandler(line,i) {
         let variableRegex = /^var|const .* = .*$/i;
-        // Object.keys(this.vars).forEach(key=>{
-        //     if(line.indexOf(key)!==-1){
-        //         line = line.replace(key, this.vars[key].value)
-        //     }
-        // })
+        var dontError = false;
+        Object.keys(this.vars).forEach(key=>{
+            if(line.indexOf(key)!==-1){
+                // line = line.replace(key, this.vars[key].value)
+                this.vars[key].typeC.print()
+                dontError=true;
+            }
+        })
         if(variableRegex.test(line)) {
             this.variableHandler(line)
         } else {
-            if (this.options.indexOf('dev:true')!==-1) {
-                console.log(Error(`Unexpected Expression`).message+' LINE: '+line)
-            } else {
-                throw Error(`Unexpected Expression`)
+            var lineType = new Type(line)
+            if(lineType.main('silence')!==`Not valid type!`) {
+                lineType.print()
+            } else if (!dontError) {
+                if (this.options.indexOf('dev:true')!==-1) {
+                    console.log(Error(`Unexpected Expression`).message+' LINE: '+line + ' index: '+i)
+                } else {
+                    console.log(Error(`\nERROR: Unexpected Expression\n\t Line Contents: ${line}\n\t Line Number: ${i+1}`).message)
+                }
             }
         }
     }
@@ -52,16 +61,24 @@ class Interpreter {
         }
         const indexOfType = line.indexOf(var_type)+var_type.length+1;
         const indexOfAssigner = line.indexOf('=');
-        const var_name = line.substring(indexOfType,indexOfAssigner);
+        const var_name = line.substring(indexOfType,indexOfAssigner-1);
         var var_value = line.substring(indexOfAssigner+1)
-        var_value=var_value.replace(' ', '')
-        this.vars[var_name] = new Variable(var_name,var_value,var_type)
+        for(let i=0;i<5;i++){
+            var_value=var_value.replace(' ', '')
+        }
+        if(this.vars[var_name]){
+            this.vars[var_name].setValue(var_value)
+        } else {
+            this.vars[var_name] = new Variable(var_name,var_value,var_type)
+        }
     }
 
     main() {
         const lines = this.file.split('\n')
+        var i=0
         lines.forEach(line=>{
-            this.lineTypeHandler(line)
+            this.lineTypeHandler(line,i)
+            i++
         })
     }
 }
@@ -73,5 +90,5 @@ const opt = pvg[pvgl-2];
 if (pvg[pvgl-1].indexOf('.g')!==-1) {
     new Interpreter(pvg[pvgl-1], opt);
 } else {
-    throw Error(`The argument at position [-1] does not contain ".g"! FATAL`)
+    console.log('\n\t'+Error(`ERROR FATAL: The argument at position [-1] is not a glang file (.g).\n`).message)
 }
