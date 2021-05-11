1cb219da2c134a2e7910ea894425b466b90dc62e
glaukiol1 <glaukiol1@gmail.com>
2021-05-11 20:40:39 +0200

    Major imporovements

--- src/glang.js
+++ src/glang.js
@@ -7,8 +7,13 @@
  * to run a .g file.
  */
 
-const fs = require('fs')
+
+/*
+    Include Modules
+*/
 const path = require('path')
+const fs = require('fs')
+const Variable = require('./Variable')
 
 class Interpreter {
     constructor(file_name,options) {
@@ -16,16 +21,22 @@ class Interpreter {
         this.options.indexOf('verbose')!==-1?console.log('Reading File'):''
         this.file = fs.readFileSync(path.join(__dirname, file_name), 'utf8')
         this.options.indexOf('verbose')!==-1?console.log('Read File'):''
+        this.vars = {} // Define the variables holder
         this.main()
     }
 
     lineTypeHandler(line) {
         let variableRegex = /^var|const .* = .*$/i;
+        // Object.keys(this.vars).forEach(key=>{
+        //     if(line.indexOf(key)!==-1){
+        //         line = line.replace(key, this.vars[key].value)
+        //     }
+        // })
         if(variableRegex.test(line)) {
             this.variableHandler(line)
         } else {
             if (this.options.indexOf('dev:true')!==-1) {
-                console.log(Error(`Unexpected Expression`).message)
+                console.log(Error(`Unexpected Expression`).message+' LINE: '+line)
             } else {
                 throw Error(`Unexpected Expression`)
             }
@@ -39,7 +50,12 @@ class Interpreter {
         } else if (line.indexOf('const')!==-1) {
             var_type='const'
         }
-        console.log(var_type)
+        const indexOfType = line.indexOf(var_type)+var_type.length+1;
+        const indexOfAssigner = line.indexOf('=');
+        const var_name = line.substring(indexOfType,indexOfAssigner);
+        var var_value = line.substring(indexOfAssigner+1)
+        var_value=var_value.replace(' ', '')
+        this.vars[var_name] = new Variable(var_name,var_value,var_type)
     }
 
     main() {
